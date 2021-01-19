128898cdf97115d0cdf9070a2fff630711901d4a
Jessica Sestak <jsestak2@mail.greenriver.edu>
2021-01-19 15:22:35 -0500

    Revert "Added home page view"
    This reverts commit 06952e2c759c1aae4c64107d5e18d6a94e61f9a7.

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-
-
 //Turn on error reporting -- this is critical!
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -11,13 +9,6 @@ require_once('vendor/autoload.php');
 //Create an instance of the Base class
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
-
-//Define a default route
-$f3->route('GET /', function() {
-    $view = new Template();
-    echo $view->render('views/home.html');
-});
-
 
 //Run fat free
 $f3->run();
